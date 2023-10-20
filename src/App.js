8e2109c71d42c38a1dcd2fd3c49e60de0283e3e7
raksha0709 <rakshakunder9@gmail.com>
2023-10-20 10:05:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function App() {
+const App = () => {
+  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+  const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
+  const stopListening = () => SpeechRecognition.stopListening();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Speech to text Converter</h2>
+      </div>
+      <div className="main-content">
+        {transcript}
+      </div>
+      <div className="btn-style">
+        <button onClick={startListening}>Start</button>
+        <button onClick={stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
